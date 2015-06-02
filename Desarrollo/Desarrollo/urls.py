ff54c30de1b8b774da6d1ee2890c4667112db6da
zakio0403 <luisfer0403@gmail.com>
2015-06-02 09:29:33 -0500

    otro cambios pendejos

--- Desarrollo/Desarrollo/urls.py
+++ Desarrollo/Desarrollo/urls.py
@@ -21,7 +21,6 @@ urlpatterns = patterns('',
   #      {'template_name' : 'ingreso.html'}, name = 'login'),
     url(r'^registro/$', 'CierSur.views.addDataPersonForm', name='registro'),
      url(r'^registro/academicHistory/$', 'CierSur.views.addAcademicHystory', name='registroAcademic'),   #Resgistro de la informacion academica
-
     url(r'^academicHistory/$', 'CierSur.views.addAcademicHystory', name='registroAcademic'),   #Resgistro de la informacion academica
     url(r'^curso_estudiante/$', 'CierSur.views.curso_estudiante', name='curso_estudiante'),   #Resgistro de la informacion academica
     url(r'^calificar/$', 'CierSur.views.curso_calificar', name='curso_calificar'),
